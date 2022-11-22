fd6bd4bff1dd20a50c879013b8d74e6c28f052f9
alejamundo <higuta47@gmail.com>
2022-11-22 17:49:17 -0500

    Finalizacion JS1

--- ejercicios-Henrry/JS1.js
+++ ejercicios-Henrry/JS1.js
@@ -148,77 +148,87 @@ console.log(elevar(5,10));
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
-  
+  return Math.floor(num);
 }
-
+console.log(redondearNumero(9.3));
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
-  
+  return Math.round()
 }
-
+console.log(redondearHaciaArriba(9.5));
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
-  
+  return Math.random();
 }
-
+console.log(numeroRandom());
 function esPositivo(numero) {
   //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  let mensaje="";
+  if (numero>0) {
+    mensaje="El numero es positivo"
+  }else if (numero<0)  {
+    mensaje ="el numero es negativo"
+  }else{
+    return false
+  }
+  return mensaje;
 }
-
+console.log(esPositivo(-1))
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return str+"!"
 }
-
+console.log(agregarSimboloExclamacion("hola mundo"))
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  return nombre+" "+apellido;
 }
-
+console.log(combinarNombres("Alejandra","orrego"));
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  return `Hola ${nombre}!`;
 }
+console.log(obtenerSaludo("Martin"));
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  return alto*ancho
 }
-
+console.log(obtenerAreaRectangulo(5,4));
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  return lado*4
 }
-
-
+console.log(retornarPerimetro(4));
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+    return (base*altura)/2
 }
+console.log(areaDelTriangulo(4,6));
 
 
 function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  return euro*1.20;
 }
-
+console.log(deEuroAdolar(5));
 
 function esVocal(letra){
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
@@ -226,11 +236,12 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+ if(letra=='a'||letra=='e'||letra=='i'||letra=='o'||letra=='u'){
+    return "Es vocal"
+  }else if (letra.length>1){
+    return "Dato incorrecto solo es un caracter"
+  }else{
+    return "Dato incorrecto no es vocal"
+  }
 }
-
-
-
-// No modificar nada debajo de esta línea
-// --------------------------------
-
+console.log(esVocal('o'));
